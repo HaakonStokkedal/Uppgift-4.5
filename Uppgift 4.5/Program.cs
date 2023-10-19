eb01ff9514b10a9365b7ee2d798b21ec64f59555
hast06001 <hast06001@E-PW024NW1>
2023-10-19 21:19:11 +0200

    Add project files.

---
+++ Uppgift 4.5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uppgift_4._5
+{
+    class Prgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            string tal = Console.ReadLine();
+
+            for (int i = 0; i < tal.Length; i++)
+            {
+                string tecken = tal[i].ToString();
+                int siffra = int.Parse(tecken);
+                if(siffra == 9)
+                {
+                    Console.Write(0);
+                }
+                else
+                {
+                    Console.Write(siffra + 1);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
